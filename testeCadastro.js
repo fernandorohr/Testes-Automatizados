d5811619fc2fe08b23a7194709d6ad7a793d3bef
Alison Lamb <alisonlamb@sou.faccat.br>
2020-01-30 15:20:41 -0200

    teste cadastro finalizado

--- testeCadastro.js
+++ testeCadastro.js
@@ -1,22 +1,54 @@
 ///<reference types="Cypress" />
 
-import Chance from 'chance';
-const chance = new Chance();
+var Chance = require('chance');
+var chance = new Chance();
 
+var randomstring = require("randomstring");
+
+var randomEmail = require('random-email');
+
+const CPF = require('cpf-check');
 
 describe("efetuar cadastro", function(){
     it("efetuar cadastro", function(){
         cy.visit("https://www.panvel.com")
+        cy.wait(1000)
         cy.reload(true)
-        cy.wait(500)
+        cy.wait(1000)
         cy.get('span[class="navbar__menu-login--text"]').click()
         cy.wait(1000)
         cy.contains("Criar conta").click()
-        cy.get('input[name="nome"]').type(chance.name)
-        cy.get('input[name="cpf"]').type(chance.cpf)
-        cy.get('input[id="input-email"]').type(chance.email)
-        cy.get('input[name="senha"]').type('ISAAC')
-        cy.get('input[name="confirmarSenha"]').type('ISAAC')
-        cy.get('button[id="btn-next-step"]').click()
+        cy.wait(1000)
+        cy.get('input[name="nome"]').type(randomstring.generate(7))
+        cy.wait(500)
+        cy.get('input[name="cpf"]').type(CPF.generate())
+        cy.wait(500)
+        cy.get('input[id="input-email"]').type(randomEmail({ domain: 'teste.com' }))
+        cy.wait(500)
+        cy.get('input[name="senha"]').type('ISAAC123')
+        cy.get('input[name="confirmarSenha"]').type('ISAAC123')
+        
+        cy.contains("Continuar").click()
+
+        cy.wait(500)
+        cy.get('input[id="input-data-nascimento"]').type('04051990')
+
+        cy.get('input[id="input-ddd"]').type('51')
+
+        cy.get('input[id="input-phone"]').type('999999999')
+
+        cy.contains("Continuar").click()
+
+        cy.get('input[name="enderecoIdentificador"]').type('casa')
+
+        cy.get('input[id="input-cep"]').type('04843-425')
+
+        cy.get('input[id="input-enderecoNumero"]').type('987')
+
+        cy.contains("Continuar").click()       
+
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
     })
 })
