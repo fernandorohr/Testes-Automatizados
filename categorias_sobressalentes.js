a8f41310dfb2761c53f41fa0a95274bbc3987716
IsJordanBraz <jordanbraz@sou.faccat.br>
2020-01-31 11:33:59 -0300

    Atualizado para o novo html e removido categoria ofertas

---
+++ categorias_sobressalentes.js
@@ -0,0 +1,36 @@
+describe('Categorias Sobressalentes', function () {
+    var tamanhoSubCategoria;
+    it('Acessa Panvel', function () {
+        cy.visit('https://www.panvel.com/panvel/main.do')
+    })    
+    it('Categoria Conveniência', function () {
+        cy.get('[data-subcategory="31181"] ul li').then(($subCategoria) => {
+            tamanhoSubCategoria = $subCategoria.length;
+            for (let i = 0; i < tamanhoSubCategoria; i++) {
+                cy.subCategoria(i, 31181)
+                cy.wait(500)
+            }
+        })
+    })
+    it('Categoria Saúde Sexual', function () {
+        cy.get('[data-subcategory="32414"] ul li').then(($subCategoria) => {
+            tamanhoSubCategoria = $subCategoria.length;
+            for (let i = 0; i < tamanhoSubCategoria; i++) {
+                cy.subCategoria(i, 32414)
+                cy.wait(500)
+            }
+        })
+    })
+    it('Categoria Cuidados com Pet', function () {
+        cy.get('[data-subcategory="33626"] ul li').then(($subCategoria) => {
+            tamanhoSubCategoria = $subCategoria.length;
+            for (let i = 0; i < tamanhoSubCategoria; i++) {
+                cy.subCategoria(i, 33626)
+                cy.wait(500)
+            }
+        })
+    })
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false
+    })
+})
