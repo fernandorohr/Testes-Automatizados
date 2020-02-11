1bf435c717eea72db8ce4dbc6fec6c0f0467a879
otavioPohren <otaviopohren@sou.faccat.br>
2020-02-11 11:20:42 -0300

    compra retirar na loja

--- testesEstruturados/cypress/support/elements/CompraRetirarNaLojaElements.js
+++ testesEstruturados/cypress/support/elements/CompraRetirarNaLojaElements.js
@@ -3,12 +3,12 @@ class CompraRetirarNaLojaElements {
     withdrawShop = () => {return 'button[data-entrega="RETIRADA_BALCAO"]'}
     city = () => {return 'select[name="select-cidade-loja"]'}
     shopName = () => {return 'button[data-nome-filial="PANVEL NOVA DE TAQUARA"]'}
-    confirmWithdra = () => {return 'button[data-click="confirmarEntregaBalcao"]'}
+    confirmWithdrawShop = () => {return 'button[data-click="confirmarEntregaBalcao"]'}
     checkoutPay = () => {return 'button[data-click="checkout-pagamento"]'}
     creditCard = () => {return 'button[data-pagamento="CARTAO_CREDITO"]'}
     creditCardNumber = () => {return 'input[id="input-card-number"]'}
-    creditCardExpith = () => {return 'select[name="input-card-month-expiry"]'}
-    creditCardExpir = () => {return 'select[name="input-card-expiry"]'}
+    creditCardExpiryMonth = () => {return 'select[name="input-card-month-expiry"]'}
+    creditCardExpiryYear = () => {return 'select[name="input-card-expiry"]'}
     creditCardCVC = () => {return 'input[name="input-card-cvc"]'}
     confirm = () => {return 'button[id="btn-finalizar-compra"]'}
 }
--- testesEstruturados/cypress/support/pageobjects/CompraRetirarNaLojaPage.js
+++ testesEstruturados/cypress/support/pageobjects/CompraRetirarNaLojaPage.js
@@ -5,23 +5,26 @@ import GlobalElements from '../elements/GlobalElements'
 const compraRetirarNaLojaElements = new CompraRetirarNaLojaElements
 const globalElements = new GlobalElements
 
-class CompraRetirarNaLojaPage{
-    retirarNaLoja(){
+class CompraRetirarNaLojaPage {
+    retirarNaLoja() {
         cy.get(globalElements.delivery()).click()
         cy.get(compraRetirarNaLojaElements.withdrawButton()).click()
         cy.get(compraRetirarNaLojaElements.withdrawShop()).click()
         cy.get(compraRetirarNaLojaElements.city()).select('TAQUARA')
         cy.get(compraRetirarNaLojaElements.shopName()).click()
-        cy.get(compraRetirarNaLojaElements.confirmWithdrawShop().click())
+        cy.get(compraRetirarNaLojaElements.confirmWithdrawShop()).click()
         cy.get(compraRetirarNaLojaElements.checkoutPay()).click()
+        cy.wait(1000)
         cy.get(compraRetirarNaLojaElements.creditCard()).click()
+        cy.wait(1000)
         cy.get(compraRetirarNaLojaElements.creditCardNumber()).type('4916823881703714')
-        cy.get(compraRetirarNaLojaElements.creditCardExpiryMonth()).type('05')
-        cy.get(compraRetirarNaLojaElements.creditCardExpiryYear()).type('2020')
+        cy.get(compraRetirarNaLojaElements.creditCardExpiryMonth()).select('05')
+        cy.get(compraRetirarNaLojaElements.creditCardExpiryYear()).select('2020')
         cy.get(compraRetirarNaLojaElements.creditCardCVC()).type('297')
         cy.contains('CONFIRMAR PAGAMENTO').click()
         cy.wait(1000)
-        cy.get(compraRetirarNaLojaElements.confirm().click())
+        cy.get(compraRetirarNaLojaElements.confirm()).click()
+
 
     }
 
